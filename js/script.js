44eb67a96d45814afcd546aa67d3bcf7d1dad935
Eduardo Henrique <duardohenriqe19@gmail.com>
2022-12-03 12:04:22 -0300

    js e plugin

---
+++ js/script.js
@@ -0,0 +1,81 @@
+
+// ativar links do menu 
+const links = document.querySelectorAll('.header-menu a');
+
+function ativarLink(link) {
+    const url = location.href;
+    const href = link.href;
+
+
+    if(url.includes(href)) {
+        link.classList.add('ativo');
+        console.log(link)
+    }
+
+}
+
+links.forEach(ativarLink);
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+    const elemento = document.getElementById(parametro);
+    if(elemento) {
+        elemento.checked = true;
+    }
+}
+
+parametros.forEach(ativarProduto);
+
+
+// perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+    const pergunta = event.currentTarget;
+    const controls = pergunta.getAttribute('aria-controls');
+    const resposta = document.getElementById(controls);
+    
+
+    resposta.classList.toggle("ativa");
+    const ativa = resposta.classList.contains("ativa");
+    pergunta.setAttribute('aria-expanded', 'ativa')
+    console.log(resposta)
+}
+
+function eventosPerguntas(pergunta) {
+    pergunta.addEventListener('click',ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+
+// galeria de bikcrafts
+
+const galeria = document.querySelectorAll('.bicicletas-imagem img');
+const galeriaContainer = document.querySelector('.bicicletas-imagem');
+
+function trocarImagem(event) {
+    const img = event.currentTarget;
+    const media = matchMedia('(min-width: 1000px)').matches;
+    if (media) {
+        galeriaContainer.prepend(img);
+    }
+    
+}
+
+function eventosGaleria(img) {
+    img.addEventListener('click', trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+
+// animação 
+if(window.SimpleAnime) {
+    new SimpleAnime();
+}
+
